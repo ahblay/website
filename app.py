3c843683fbadaaa10238e514726abd166e6825ec
Abel Romer <abel.romer@questu.ca>
2019-05-21 00:46:05 -0700

    Adds jupyter HTML files and some anecdotes.

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/p1.html')
-def projects():
-    return render_template('p1.html')
+@app.route('/<string:page_name>/')
+def projects(page_name):
+    return render_template('%s.html' % page_name)
 
 
 if __name__ == '__main__':
--- freeze.py
+++ freeze.py
@@ -3,5 +3,13 @@ from app import app
 
 freezer = Freezer(app)
 
+@freezer.register_generator
+def projects():
+    yield {'page_name': 'j1'}
+    yield {'page_name': 'j2'}
+    yield {'page_name': 'w1'}
+    yield {'page_name': 'w2'}
+    yield {'page_name': 'w3'}
+
 if __name__ == '__main__':
     freezer.freeze()
