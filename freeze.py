89395e8434c6bd737cc16fd4676a4a15171bc285
Abel Romer <abel.romer@questu.ca>
2019-05-23 12:51:37 -0700

    Adds a review of Athena's omelette.

--- freeze.py
+++ freeze.py
@@ -10,6 +10,7 @@ def projects():
     yield {'page_name': 'w1'}
     yield {'page_name': 'w2'}
     yield {'page_name': 'w3'}
+    yield {'page_name': 'r1'}
 
 if __name__ == '__main__':
     freezer.freeze()
