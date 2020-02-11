f67224ced89d082cacf078357b31857a9083eba3
Abel Romer <abel.romer@questu.ca>
2020-02-10 19:32:20 -0800

    Updates modal functionality to work for all images of class .photo.

--- static/modal.js
+++ static/modal.js
@@ -1,18 +1,13 @@
-// Get the modal
-var modal = document.getElementById("schedule-modal");
+$(".photo").click(function() {
+    console.log(this);
+    var modalId = this.id + "-modal";
+    var modalImgSrc = this.src;
+    console.log(modalId);
+    console.log(modalImgSrc);
+    $("#" + modalId).css("display", "block");
+    $("#" + modalId).find("img").attr("src", modalImgSrc);
+})
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("schedule-calendar");
-var modalImg = document.getElementById("modal-content");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+$(".close").click(function() {
+  $(this).parent().css("display", "none");
+})
